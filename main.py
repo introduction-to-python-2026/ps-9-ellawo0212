d5efcfb865761631679e9000219d079564ffae69
ellawo0212 <ellawo0212@gmail.com>
2026-01-06 23:16:28 +0200

    Change KNeighborsClassifier neighbors from 60 to 30

--- main.py
+++ main.py
@@ -18,13 +18,12 @@ x=Scalar.fit_transform(x)
 x_train , x_test , y_train , y_test =train_test_split(x,y, test_size=0.2, random_state=42, stratify=y )
 KN_model = Pipeline([
     ('scaler', MinMaxScaler()),
-    ('knn', KNeighborsClassifier(n_neighbors=60))
+    ('knn', KNeighborsClassifier(n_neighbors=30))
 ])
-KN_model=KNeighborsClassifier(n_neighbors=60)
+KN_model=KNeighborsClassifier(n_neighbors=30)
 KN_model.fit(x_train , y_train)
 Y_pred = KN_model.predict(x_test)
 accuracy= accuracy_score(y_test, Y_pred)
 print("accuracy:",accuracy)
 import joblib
-
 joblib.dump(KN_model, 'my_model.joblib')
