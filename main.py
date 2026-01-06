7fc18fbfdad4cf1f01e8d574f5e591f38f3f1aa8
ellawo0212 <ellawo0212@gmail.com>
2026-01-06 23:03:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.pipeline import Pipeline
 df = pd.read_csv("parkinsons.csv")
 df = df.dropna()
 sns.pairplot(df, hue="status", diag_kind="kde", corner=True)
@@ -15,6 +16,10 @@ y = df["status"]
 Scalar=MinMaxScaler()
 x=Scalar.fit_transform(x)
 x_train , x_test , y_train , y_test =train_test_split(x,y, test_size=0.2, random_state=42, stratify=y )
+KN_model = Pipeline([
+    ('scaler', MinMaxScaler()),
+    ('knn', KNeighborsClassifier(n_neighbors=60))
+])
 KN_model=KNeighborsClassifier(n_neighbors=60)
 KN_model.fit(x_train , y_train)
 Y_pred = KN_model.predict(x_test)
